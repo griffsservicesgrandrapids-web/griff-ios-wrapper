6ed9b30f77f3aa084aeb25d8f702847f25368afd
griffsservicesgrandrapids-web <griffsservicesgrandrapids@gmail.com>
2026-03-13 22:04:35 -0400

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.griff.gigconnect',
+  appName: 'Gig Connect',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
