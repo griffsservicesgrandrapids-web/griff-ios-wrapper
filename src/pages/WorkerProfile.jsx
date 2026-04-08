64543ff2fb414952c6c45c18018406da6d1c9581
griffsservicesgrandrapids-web <griffsservicesgrandrapids@gmail.com>
2026-04-07 23:35:15 -0400

    Update WorkerProfile.jsx

--- src/pages/WorkerProfile.jsx
+++ src/pages/WorkerProfile.jsx
@@ -141,7 +141,7 @@ export default function WorkerProfile() {
       } else {
         toast.success('Profile created! Awaiting admin approval.');
       }
-      navigate(createPageUrl('WorkerDashboard'));
+      navigate(createPageUrl('worker-dashboard'));
     }
   });
 
